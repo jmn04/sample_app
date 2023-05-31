421b8054bf556cae6f85ce1cc69d7dd658a28051
EC2 Default User <ec2-user@ip-172-31-36-89.ap-northeast-1.compute.internal>
2023-05-31 02:12:39 +0000

    [Add] 投稿機能

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,17 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
   end
+  
+  def create
+    # データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+
 
   def index
   end
@@ -9,5 +20,11 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
